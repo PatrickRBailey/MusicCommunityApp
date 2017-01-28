4ceabc4a7f50e2f69fefd9aeabcff95de0959c7a
Patrick Bailey <Patrick Bailey>
2017-01-28 10:40:50 -0800

    initial commit

---
+++ src/MusicCommunityApp/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CommunityWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Date = DateTime.Now.ToString("MM/dd/yyyy");
+            return View();
+        }
+
+        public ViewResult About()
+        {
+            return View();
+        }
+        [Route("Home/About/Contact")]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ViewResult History()
+        {
+            return View();
+        }
+    }
+}
