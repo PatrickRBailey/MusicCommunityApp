ba7b032e4030922ce074fb32f43224b9d84d41ad
Patrick Bailey <patrickbailey13@gmail.com>
2017-01-28 13:04:47 -0800

    wrote new tests

--- MusicCommunity.Tests/NewsTests.cs
+++ MusicCommunity.Tests/NewsTests.cs
@@ -1,5 +1,6 @@
 ﻿using MusicCommunityApp.Models;
 using Xunit;
+using System;
 
 namespace MusicCommunity.Tests
 {
@@ -15,6 +16,46 @@ namespace MusicCommunity.Tests
             n.Title = title;
             // Assert
             Assert.Equal(title, n.Title);
+        }
+
+        [Fact]
+        public void CanGetDate() 
+        {
+            //Arrange
+            var date = DateTime.Now;
+            var n = new News {Title = "Test", Date = date};
+
+            //Act
+
+            //Assert
+            Assert.Equal(n.Date, date);
+        }
+
+        [Fact]
+        public void CanGetStory()
+        {
+            //Arrange
+            var story = "This is my story";
+            var n = new News { Title = "Test", Date = DateTime.Now, Story = story };
+            //Act
+
+            //Assert
+            Assert.Equal(n.Story, story);
+        }
+
+        [Fact]
+        public void CanChangeStory()
+        {
+            //Arrange
+            var story = "This is my story";
+            var newStory = "This is a changed story";
+            var n = new News { Title = "Test", Date = DateTime.Now, Story = story };
+
+            //Act
+            n.Story = newStory;
+
+            //Assert
+            Assert.Equal(n.Story, newStory);
         }
     }
 }
