f71ebb45fe2999114b4e02e1f0f0789f77973a21
Patrick Bailey <Patrick Bailey>
2017-01-28 10:53:38 -0800

    added News Model

---
+++ src/MusicCommunityApp/Models/News.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace CommunityWebsite.Models{
+    public class News {
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Story { get; set; }
+    }
+}
