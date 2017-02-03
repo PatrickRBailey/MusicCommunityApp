a6818fa8ec8326e233c0c7aafd6352362ea73c8c
Patrick Bailey <Patrick Bailey>
2017-02-02 16:33:05 -0800

    added a repository and interface

---
+++ src/MusicCommunityApp/Repositories/IMember.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MusicCommunityApp.Models;
+
+namespace MusicCommunityApp.Repositories
+{
+    public interface IMember
+    {
+        Member GetMemberByName(string LastName, string FirstName);
+        Member GetMemberByMessage(Message message);
+        List<Member> GetAllMembers();
+
+    }
+}
---
+++ src/MusicCommunityApp/Repositories/MemberRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MusicCommunityApp.Models;
+
+namespace MusicCommunityApp.Repositories
+{
+    public class MemberRepository : IMember
+    {
+        public List<Member> GetAllMembers()
+        {
+            var members = new List<Member>();
+            members.Add(new Member() { FirstName = "Patrick", LastName = "Bailey",
+                Email = "patrickb@blahblah.org" });
+            members.Add(new Member()
+            {
+                FirstName = "Gob",
+                LastName = "Bluth",
+                Email = "gbluth@blahblah.org"
+            });
+
+            return members;
+        }
+
+        public Member GetMemberByMessage(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Member GetMemberByName(string LastName, string FirstName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
