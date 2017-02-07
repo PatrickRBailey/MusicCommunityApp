e157744c09173c5a2c1c7870a40b42c1d87a409e
Patrick Bailey <patrickbailey13@gmail.com>
2017-02-06 21:34:30 -0800

    controller methods for messages

--- src/MusicCommunityApp/Controllers/Forum.cs
+++ src/MusicCommunityApp/Controllers/Forum.cs
@@ -1,13 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
+using MusicCommunityApp.Repositories;
+using MusicCommunityApp.Models;
 
 namespace MusicCommunityApp.Controllers
 {
     public class ForumController : Controller
     {
-      
+        private IMessage messageRepo;
+        private IMember memberRepo;
+        private Member user = new Member(){FirstName="Luke", LastName="Skywalker", Email="usetheforce@gmail.com"};
+        public ForumController(IMessage messRepo, IMember memRepo)
+        {
+            messageRepo = messRepo;
+            memberRepo = memRepo;
+        }
         public ViewResult Index()
-    {
-        return View();
+        {
+            return View();
+        }
+
+        public ViewResult AllMessages()
+        {
+            var repo = new MessageRepository();
+            var messages = repo.GetAllMessages();
+            return View(messages);
+        }
+
+        public ViewResult MyMessages()
+        {
+            var repo = new MessageRepository();
+            var messages = repo.GetMessagesForMember(user);
+            return View(messages);
+        }
+
+
     }
-  }
 }
