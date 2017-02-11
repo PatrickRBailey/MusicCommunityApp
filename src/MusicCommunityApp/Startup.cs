ad044f0ab9fe4b5c2f0ba5e6165a7b601d688ca8
Patrick Bailey <patrickbailey13@gmail.com>
2017-02-11 13:14:35 -0800

    configured startup for database

--- src/MusicCommunityApp/Startup.cs
+++ src/MusicCommunityApp/Startup.cs
@@ -8,29 +8,31 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using MusicCommunityApp.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace MusicCommunityApp
 {
     public class Startup
     {
+        IConfigurationRoot Configuration;
         public Startup(IHostingEnvironment env)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json").Build();
         }
+        
 
-        public IConfigurationRoot Configuration { get; }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
             services.AddMvc();
-            services.AddTransient<IMember, MemberRepository>();
+            services.AddDbContext<ApplicationDbContext>(opts => opts.UseNpgsql(
+                Configuration["DbContextSettings:ConnectionString"]));
             services.AddTransient<IMessage, MessageRepository>();
         }
 
@@ -58,6 +60,7 @@ namespace MusicCommunityApp
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+            SeedData.EnsurePopulated(app);
         }
     }
 }
