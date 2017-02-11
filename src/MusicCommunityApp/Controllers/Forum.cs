d60b16f73b234e8150621c36cc83d8928835596d
Patrick Bailey <patrickbailey13@gmail.com>
2017-02-11 13:26:23 -0800

    added init migration and updated database

--- src/MusicCommunityApp/Controllers/Forum.cs
+++ src/MusicCommunityApp/Controllers/Forum.cs
@@ -21,21 +21,12 @@ namespace MusicCommunityApp.Controllers
 
         public ViewResult AllMessages()
         {
-            var repo = new MessageRepository();
-            var messages = repo.GetAllMessages();
-            return View(messages);
+            return View(repository.GetAllMessages());
         }
 
-        public ViewResult MyMessages()
+        public ViewResult MyMessages(Member me)
         {
-            MemberRepository memberRepo = new MemberRepository();
-            MemberController controller = new MemberController(memberRepo);
-            List<Member> members = controller.AllMembers().ViewData.Model as List<Member>;
-
-            Member user = members[0];
-            var repo = new MessageRepository();
-            var messages = repo.GetMessagesForMember(user);
-            return View(messages);
+            return View(repository.GetMessagesForMember(me));
         }
 
        
---
+++ src/MusicCommunityApp/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MusicCommunityApp.Repositories;
+
+namespace MusicCommunityApp.Migrations
+{
+    [DbContext(typeof(ApplicationDbContext))]
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431");
+
+            modelBuilder.Entity("MusicCommunityApp.Models.Member", b =>
+                {
+                    b.Property<int>("MemberID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Email");
+
+                    b.Property<string>("FirstName");
+
+                    b.Property<string>("LastName");
+
+                    b.HasKey("MemberID");
+
+                    b.ToTable("Members");
+                });
+
+            modelBuilder.Entity("MusicCommunityApp.Models.Message", b =>
+                {
+                    b.Property<int>("MessageID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Body");
+
+                    b.Property<DateTime>("Date");
+
+                    b.Property<string>("Event");
+
+                    b.Property<int?>("FromMemberID");
+
+                    b.Property<string>("Subject");
+
+                    b.HasKey("MessageID");
+
+                    b.HasIndex("FromMemberID");
+
+                    b.ToTable("Messages");
+                });
+
+            modelBuilder.Entity("MusicCommunityApp.Models.Message", b =>
+                {
+                    b.HasOne("MusicCommunityApp.Models.Member", "From")
+                        .WithMany()
+                        .HasForeignKey("FromMemberID");
+                });
+        }
+    }
+}
